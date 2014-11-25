0066790dcd7470aeed753d74e4ed25eb08025851
arshias <arshiasingh@berkeley.edu>
2014-11-25 10:12:01 -0800

    Create reducer_gender_1.py

---
+++ reducer_gender_1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import sys
+
+
+current_uid = None
+current_click = 0
+current_impression = 0
+current_gender = None
+
+
+for line in sys.stdin:
+
+    # remove entrailing white spaces
+    line = line.strip()
+
+    # split line to variables
+    uid, click, impression, gender = line.split('\t')
+
+
+    if current_uid == uid:
+
+        # if data exists, cumulate
+        if click != "-1" and impression != "-1":
+            current_click += float(int(click))
+            current_impression += float(int(impression))
+
+        # each user should have one gender
+        if gender != "-1":
+            current_gender = gender
+
+    else:
+        if current_uid:
+            # print stdout
+            # print '%s\t%s\t%s\t%s' % (current_uid, current_gender, current_click, current_impression)
+            print '%s\t%s\t%s' % (current_gender, current_click, current_impression)
+        # reset parameters
+        if click != "-1" and impression != "-1":
+            current_click = float(int(click))
+            current_impression = float(int(impression))
+        else:
+            current_click = 0
+            current_impression = 0
+        current_gender = gender
+        current_uid = uid
+
+# print last line
+if current_uid:
+    # print '%s\t%s\t%s\t%s' % (current_uid, current_gender, current_click, current_impression)
+    print '%s\t%s\t%s' % (current_gender, current_click, current_impression)
