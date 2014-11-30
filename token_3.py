5631dfc01d2e7c19a764d25dc0511a80ef949f55
arshias <arshiasingh@berkeley.edu>
2014-11-30 00:23:49 -0800

    Create token_3.py

---
+++ token_3.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import sys
+
+for line in sys.stdin:
+
+    query_token = line[3]
+    title_token = line[1]
+    
+    qtokens = query_token.strip('|')
+    ttoken = title_token.strip('|')
+
+    ### This loops through the ttok with the qtok to find and return
+    ### any matching values as a list
+    matched = list(set(qtokens).intersection(ttokens))
+
+    ### len(qtok) tell us how many tokens are in the query
+    ratio = float(len(matched))/float(len(qtokens))
+
+    print '%s\t%s\t%s\t' % (line[0], line[2], ratio, line[4], line[5])
+    #prints titleid, queryid, ratio, click, impressions
