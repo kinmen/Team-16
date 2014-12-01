accb64dd9751622abd55a95ae9a5f1218035e232
kinmen <sparkstar6142@berkeley.edu>
2014-12-01 14:42:55 -0800

    Update token_3.py

--- token_3.py
+++ token_3.py
@@ -17,5 +17,5 @@ for line in sys.stdin:
     ### len(qtok) tell us how many tokens are in the query
     ratio = float(len(matched))/float(len(qtokens))
 
-    print '%s\t%s\t%s\t%s' % (ratio, 'simil', line[4], line[5])
+    print '%s\t%s\t%s\t%s' % (ratio, 'simi', line[4], line[5])
     #prints ratio, 'simil', clicks, impressions
