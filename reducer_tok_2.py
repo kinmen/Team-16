f44131e1f5f93b846068407ecd198432b7897699
arshias <arshiasingh@berkeley.edu>
2014-12-04 17:10:41 -0800

    Update reducer_tok_2.py

--- reducer_tok_2.py
+++ reducer_tok_2.py
@@ -47,7 +47,7 @@ for line in sys.stdin:
 
 
     else:
-        if current_titleid:
+        if current_titleid and current_click != 'z'::
             print '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' %  (current_keyid, current_ktoken, current_descrid, current_dtoken, current_qid, current_qtoken, current_titleid, current_ttoken, current_click, current_imp)
         current_titleid = titleid
         current_ttoken = ttoken
