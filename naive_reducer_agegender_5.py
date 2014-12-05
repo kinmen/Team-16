fc59c0704af199d2a7e81d0dd80e74ae58d6e638
kinmen <sparkstar6142@berkeley.edu>
2014-12-05 00:51:51 -0800

    red 4 and 5

--- naive_reducer_agegender_5.py
+++ naive_reducer_agegender_5.py
@@ -15,6 +15,7 @@ current_imp = 0
 current_uid = None
 current_age = None
 current_gender = None
+current_ids = []
 
 
 
@@ -25,23 +26,32 @@ for line in sys.stdin:
 
     # split for indexing
     uid, age, gender, descrid, descr_token, qid, query_token, titleid, title_token, keyid, key_token, click, impression  = line.split('\t')
-    try:
-        click = int(click)
-        impression = int(impression)
-    except ValueError:
-        continue
 
     if current_uid == uid:
         if age != "-1" and gender != "-1":
             current_age = age
             current_gender = gender
-        if click != -1 and impression != -1:
-            current_click += click
-            current_imp += impression
+        else:
+            if (current_titleid != titleid or current_keyid != keyid or current_qid != qid or current_descrid != descrid):
+                if current_click != 'z':
+                    current_ids.append((current_qid, current_qtoken, current_titleid, current_ttoken, current_keyid, current_ktoken, current_descrid, current_dtoken, current_click, current_imp, current_uid, current_age, current_gender))
+            current_titleid = titleid
+            current_ttoken = title_token
+            current_qid = queryid
+            current_qtoken = query_token
+            current_keyid = keyid
+            current_ktoken = key_token
+            current_descrid - descrid
+            current_dtoken = descr_token
+            current_click = click
+            current_imp = impression
 
     else:
-        if current_descrid:
-            print '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' %  (current_qid, current_qtoken, current_titleid, current_ttoken, current_keyid, current_ktoken, current_descrid, current_dtoken, current_click, current_imp, current_uid, current_age, current_gender)
+        if current_uid:
+            for i in current_ids:
+                f = i[:11] + (current_age, current_gender)
+                print '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' % f
+        current_ids = []
         current_titleid = titleid
         current_ttoken = title_token
         current_qid = queryid
@@ -50,11 +60,13 @@ for line in sys.stdin:
         current_ktoken = key_token
         current_descrid - descrid
         current_dtoken = descr_token
-        current_click = 0
-        current_imp = 0
+        current_click = click
+        current_imp = impression
         current_uid = uid
         current_age = age
         current_gender = gender
 
-if current_descrid:
-    print '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' %  (current_qid, current_qtoken, current_titleid, current_ttoken, current_keyid, current_ktoken, current_descrid, current_dtoken, current_click, current_imp, current_uid, current_age, current_gender)
+if current_uid:
+    for i in current_ids:
+        f = i[:11] + (current_age, current_gender)
+        print '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' % f
--- naive_reducer_tok_4.py
+++ naive_reducer_tok_4.py
@@ -15,6 +15,7 @@ current_imp = 0
 current_uid = None
 current_age = None
 current_gender = None
+current_ids = []
 
 
 
@@ -25,24 +26,35 @@ for line in sys.stdin:
 
     # split for indexing
     descrid, descr_token, qid, query_token, titleid, title_token, keyid, key_token, click, impression, uid, age, gender = line.split('\t')
-    try:
-        click = int(click)
-        impression = int(impression)
-    except ValueError:
-        continue
-    if descrid == "-1":
-        print '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' %  (qid, query_token, titleid, title_token, keyid, key_token, descrid, descr_token, click, impression, uid, age, gender)
+
+    if descrid == "z":
+        print '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' %  (qid, query_token, titleid, title_token, keyid, key_token, descrid, descr_token, click, impression, uid, age, gender)
         continue
     if current_descrid == descrid:
-        if descr_token != "-1":
+        if descr_token != 'z':
             current_dtoken = descr_token
-        if click != -1 and impression != -1:
-            current_click += click
-            current_imp += impression
+        if (current_titleid != titleid or current_keyid != keyid or current_qid != qid or current_uid != uid) and current_titleid != 'z':
+            if current_click != 'z':
+                current_ids.append((current_qid, current_qtoken, current_titleid, current_ttoken, current_keyid, current_ktoken, current_descrid, current_dtoken, current_click, current_imp, current_uid, current_age, current_gender))
+            current_titleid = titleid
+            current_ttoken = title_token
+            current_qid = qid
+            current_qtoken = query_token
+            current_keyid = keyid
+            current_ktoken = key_token
+            current_descrid = descrid
+            current_click = click
+            current_imp = impression
+            current_uid = uid
+            current_age = age
+            current_gender = gender
 
     else:
         if current_descrid:
-            print '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' %  (current_qid, current_qtoken, current_titleid, current_ttoken, current_keyid, current_ktoken, current_descrid, current_dtoken, current_click, current_imp)
+            for i in current_ids:
+                f = i[:7] + (current_dtoken,) + i[8:]
+                print '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' % f
+        current_ids = []    
         current_titleid = titleid
         current_ttoken = title_token
         current_qid = qid
@@ -51,11 +63,13 @@ for line in sys.stdin:
         current_ktoken = key_token
         current_descrid = descrid
         current_dtoken = descr_token
-        current_click = 0
-        current_imp = 0
+        current_click = click
+        current_imp = impression
         current_uid = uid
         current_age = age
         current_gender = gender
 
 if current_descrid:
-    print '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' %  (current_qid, current_qtoken, current_titleid, current_ttoken, current_keyid, current_ktoken, current_descrid, current_dtoken, current_click, current_imp)
+    for i in current_ids:
+        f = i[:7] + (current_dtoken,) + i[8:]
+        print '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' % f
