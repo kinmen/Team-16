22c99f20c3e1c3119d52d69fb3739456b14a5226
kinmen <sparkstar6142@berkeley.edu>
2014-12-05 01:02:10 -0800

    fixed format print

--- naive_mapper_agegender_5.py
+++ naive_mapper_agegender_5.py
@@ -6,4 +6,4 @@ for line in sys.stdin:
     line = line.strip()
     qid, qtoken, titleid, ttoken, keyid, ktoken, descrid, dtoken, click, imp, uid, age, gender = line.split('\t')
     #make uid as key, for gender/age
-    print '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' %  (uid, age, gender, descrid, dtoken, qid, qtoken, titleid, ttoken, keyid, ktoken, click, imp)
+    print '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s' %  (uid, age, gender, descrid, dtoken, qid, qtoken, titleid, ttoken, keyid, ktoken, click, imp, uid, age, gender)
